10e33ab4e2a54be85cf75fb905fc52e177355406
Abdul Mobarak <abdul.mobarak@engineering.digital.dwp.gov.uk>
2023-04-05 07:35:11 +0100

    purchase and redirect added

---
+++ redirect.js
@@ -0,0 +1,28 @@
+function test() {
+
+console.log("clicked");
+
+var uri = document.getElementById("uri");
+var counter = 5;
+
+setInterval(function() {
+  counter--;
+
+  if (counter >= 0) {
+    span = document.getElementById("counter");
+    span.innerHTML = counter;
+  }
+
+  // Display 'counter' wherever you want to display it.
+  if (counter === 0) {
+    //    alert('this is where it happens');
+    clearInterval(counter);
+  }
+}, 1000);
+
+window.setTimeout(function() {
+  // Move to a new location
+  window.location.href = uri.href;
+}, 5000);
+
+}
